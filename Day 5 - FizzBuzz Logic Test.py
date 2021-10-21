95cbd21ab9cd685f88720e8c9aa875b0b8ff51f7
Aaron Kidwell <aaronkidwell95@outlook.com>
2021-10-20 20:23:01 -0500

    added comments

--- Day 5 - FizzBuzz Logic Test.py
+++ Day 5 - FizzBuzz Logic Test.py
@@ -1,3 +1,10 @@
+# this is a common script logic test with a range from 1, 100
+# the goal is to print every number divisible by 3 with Fizz
+#print every number divisible by 3 and 5 with FizzBuzz
+#print every number divisible by 5 with Buzz
+#while still printing all other numbers
+
+
 for divisible in range(1, 101):
     if divisible % 3 == 0 and divisible % 5 == 0:
         print('FizzBuzz')
