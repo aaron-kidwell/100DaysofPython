f4db9f27ba9c4684d98a2e5c8eb2e1b7e0ea778a
Aaron Kidwell <aaronkidwell95@outlook.com>
2021-10-20 20:20:30 -0500

    renamed files in correct order

--- FizzBuzz Logic Test.py
+++
@@ -1,9 +0,0 @@
-for divisible in range(1, 101):
-    if divisible % 3 == 0 and divisible % 5 == 0:
-        print('FizzBuzz')
-    elif divisible % 3 == 0:
-        print('Fizz')
-    elif divisible % 5 == 0:
-        print('Buzz')
-    else:
-        print(divisible)
