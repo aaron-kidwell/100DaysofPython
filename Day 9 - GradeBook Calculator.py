218d83200ca40a34b066c0ddc740d043518dbe9f
Aaron Kidwell <aaronkidwell95@outlook.com>
2021-10-27 00:26:40 -0500

    Day 9/100 GradeBook Calculator

---
+++ Day 9 - GradeBook Calculator.py
@@ -0,0 +1,33 @@
+# Day 9/100 Gradebook Calculator
+
+# list of students in dictionary
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+# new list to store grade output
+student_grades = {}
+
+# loop through grades and assign new values taking the students names from the student_scores.
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = 'Outstanding'
+    elif score > 80:
+        student_grades[student] = 'Exceeds Expectations'
+    elif score > 70:
+        student_grades[student] = 'Acceptable'
+    else:
+        student_grades[student] = 'Fail'   
+
+
+print(student_grades)
+
+
+
+
+
