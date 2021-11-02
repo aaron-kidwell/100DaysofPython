2f97258902fa82babcb256f733cd0be1c4f95c9a
Aaron Kidwell <aaronkidwell95@outlook.com>
2021-11-01 20:49:51 -0500

    Day 12/100 Guess a Number

---
+++ Day 12 - Guess a Number/guessanumber.py
@@ -0,0 +1,39 @@
+# Day 12/100 Guess a Number
+from random import randint
+from art import logo
+print(logo)
+print("I'm thinking of a number betweeen 1 and 100.")
+#Number guessing function
+def guess_a_num():
+    number = randint(1, 100)
+    difficulty = input("Choose a difficult. Type 'easy' or 'hard': ")
+    #difficulty dictates amount of lives
+    if difficulty == 'easy':
+        lives = 10
+    else:
+        lives= 5
+    #while loop continues until continue game is false which is either lives 0 or correct guess
+    continue_game = True
+    while lives != 0 and continue_game == True:
+        
+        print(f"You have {lives} attempts remaining to guess the number.")
+        guess = int(input('Make a guess: '))
+
+        if guess > number:
+            print('Too high.')
+            lives -= 1
+        elif guess < number:
+            print('Too low.')
+            lives -= 1
+        elif guess == number:
+            print(f'You did it! The number was {number}.')
+            continue_game = False
+        else:
+            print('Incorrect input.')
+            lives -= 1
+    
+        if lives == 0:
+            print('You have ran out of lives.')
+            continue_game = False
+#call the function
+guess_a_num()
